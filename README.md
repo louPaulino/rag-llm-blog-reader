b8ed8e937e60e5c8debedefd7099a230b1fab6b5
louPaulino <lpsgyn7@gmail.com>
2024-02-23 19:07:57 -0300

    'message'

---
+++ README.md
@@ -0,0 +1,3 @@
+# Retrieval-Augmented Generation (RAG) Java Blog Reader
+Based on Intelligent Java Blog Reader
+Read the article in the Red Hat Developer blog: https://developers.redhat.com/articles/2024/01/23/how-use-llms-java-langchain4j-and-quarkus
---
+++ src/test/java/com/lpaulino/demos/ragllmjavablogreader/BlogReaderResourceTest.java
@@ -0,0 +1,23 @@
+package com.lpaulino.demos.ragllmjavablogreader;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+class BlogReaderResourceTest {
+
+    @Inject
+    WebCrawler webCrawler;
+
+    @Inject
+    RequestSplitter requestSplitter;
+
+    @Test
+    void test() {
+        var blogReaderResource = new BlogReaderResource(new FakeBlogReaderService(), webCrawler, requestSplitter);
+        assertEquals("MockGPT", blogReaderResource.read("https://www.redhat.com/en/blog/the-power-of-ai-is-open"));
+    }
+}
